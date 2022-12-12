90cc45c9553942b3f0ffb6f9290ee738e1b9ae4f
Saad5400 <sdbtwa@gmail.com>
2022-12-12 09:36:02 +0300

    .

---
+++ ProjectEtoPrototype/Controllers/ControlPanelController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectEtoPrototype.Models;
+
+namespace ProjectEtoPrototype.Controllers
+{
+    public class ControlPanelController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+            var b = new BaseModel { AllUsers = Db.Users
+                                                // .Include(u => u.Preference)
+                                                // .Include(u => u.DailyTasks)
+                                                // .Include(u => u.Bank)
+                                                // .ThenInclude(b => b.Operations)
+                                                .ToList() };
+            return View(b);
+        }
+
+        public IActionResult UserData(string userId)
+        {
+            return RedirectToAction("Index", "Home");
+            User user = GetUser(userId);
+            return View(user);
+        }
+    }
+}
---
+++ ProjectEtoPrototype/Models/BaseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEtoPrototype.Models
+{
+    public class BaseModel
+    {
+        public User? User;
+
+        public List<User>? AllUsers;
+
+    }
+}
